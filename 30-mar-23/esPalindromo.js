c08cef9019e44693903f9a63c30df692591c1599
NicolasCardozo1980 <cardozo106@gmail.com>
2023-04-10 20:01:23 -0300

    q se yo

--- 30-mar-23/esPalindromo.js
+++ 30-mar-23/esPalindromo.js
@@ -1,4 +1,7 @@
 function esPalindromo(texto) {
-    var textoReverso = texto.split('').reverse().join('').toLowerCase();
-    return texto.toLowerCase() === textoReverso;
+    var textoReverso = '';
+    for (var i = texto.length - 1; i >= 0; i--) {
+        textoReverso += texto[i];
+    }
+    return texto.toLowerCase() === textoReverso.toLowerCase();
 }
---
+++ 30-mar-23/index.ts
@@ -0,0 +1,12 @@
+function esPalindromo(texto: string): boolean {
+    const textoReverso = texto.split('').reverse().join('').toLowerCase();
+    return texto.toLowerCase() === textoReverso;
+  }
+  
+  const texto1 = 'anita lava la tina';
+  const texto2 = 'oso';
+  const texto3 = 'hola mundo';
+  
+  console.log(esPalindromo(texto1)); // true
+  console.log(esPalindromo(texto2)); // true
+  console.log(esPalindromo(texto3)); // fals  
